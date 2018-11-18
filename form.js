ac2dcefef1ccc50fe0c3a54951f82bdbe99f59c8
nazar <gntnaizer@gmail.com>
2018-11-19 00:17:27 +0200

    form1

--- form.js
+++ form.js
@@ -1,39 +1,3 @@
-// function sendform() {
-
-//     required = new array("name", "email");
-//     required_show = new array("Ваше ім’я”, “електронну адресу");
-
-//     if (document.forms[0].name.value == "") {
-//         alert("Введіть будь-ласка ваше ім'я");
-//         document.mailform.name.focus();
-//         return false;
-//     }
-
-//     if (document.forms[0].email.value == "") {
-//         alert("Введіть будь-ласка ваш мейл");
-//         document.mailform.email.focus();
-//         return false;
-//     }
-
-//     var i, j;
-
-
-//     for (j = 0; j < required.length; j++) {
-//         for (i = 0; i < document.forms[0].length; i++) {
-//             if (document.forms[0].elements[i].name == required[j] && document.forms[0].elements[i].value == "") {
-//                 alert("Будь ласка, введіть" + required_show[j]);
-//                 document.forms[0].elements[i].focus();
-//                 console.log("message allready sended");
-//                 return false;
-//             }
-//         }
-//     }
-
-//     console.log("message allready sended");
-
-//     return true;
-// }
-
 $(function() {
     $('form input[type=submit]').click(function(e) {
         const form = $('form')[0];
